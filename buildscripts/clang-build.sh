570b83bd941e2f1dcdf54d8f90254318248dae50
Dalton Woodard <daltonmwoodard@gmail.com>
2014-12-17 13:04:02 -0700

    Update to build scripts.

--- buildscripts/clang-build.sh
+++ buildscripts/clang-build.sh
@@ -1,26 +1,41 @@
 #!/bin/bash
 
 # script for travis-ci builds using clang
+
+runprogs ()
+{
+    ./test/init;
+    if [[ $? -ne 0 ]]; then echo "init failed"; return 1; fi;
+    ./test/stdstreams;
+    if [[ $? -ne 0 ]]; then echo "stdstreams failed"; return 1; fi;
+    ./examples/throughput;
+    if [[ $? -ne 0 ]]; then echo "throughput failed"; return 1; fi;
+    ./examples/synchronized;
+    if [[ $? -ne 0 ]]; then echo "synchronized failed"; return 1; fi;
+    return 0;
+}
+
+
 echo "clang c11 debug build";
 make std=c11;
-./examples/throughput;
+if [[ runprogs -ne 0 ]]; then exit 1; fi;
 tail scribedebug.log;
 make clean;
 
 echo "clang c11 regular build";
 make std=c11 OPTFLAGS=-DNDEBUG;
-./examples/throughput;
+if [[ runprogs -ne 0 ]]; then exit 1; fi;
 make clean;
 
 echo "clang c99 debug build";
 make std=c99;
-./examples/throughput;
+if [[ runprogs -ne 0 ]]; then exit 1; fi;
 tail scribedebug.log;
 make clean;
 
 echo "clang c99 regular build";
 make std=c99 OPTFLAGS=-DNDEBUG;
-./examples/throughput;
+if [[ runprogs -ne 0 ]]; then exit 1; fi;
 make clean;
 
 exit 0;
--- buildscripts/gcc-build.sh
+++ buildscripts/gcc-build.sh
@@ -2,15 +2,28 @@
 
 # script for travis-ci builds using gcc
 
+runprogs ()
+{
+    ./test/init;
+    if [[ $? -ne 0 ]]; then echo "init failed"; return 1; fi;
+    ./test/stdstreams;
+    if [[ $? -ne 0 ]]; then echo "stdstreams failed"; return 1; fi;
+    ./examples/throughput;
+    if [[ $? -ne 0 ]]; then echo "throughput failed"; return 1; fi;
+    ./examples/synchronized;
+    if [[ $? -ne 0 ]]; then echo "synchronized failed"; return 1; fi;
+    return 0;
+}
+
 echo "gcc c99 debug build";
 make std=c99;
-./examples/throughput;
+if [[ runprogs -ne 0 ]]; then exit 1; fi;
 tail scribedebug.log;
 make clean;
 
 echo "gcc c99 regular build";
 make std=c99 OPTFLAGS=-DNDEBUG;
-./examples/throughput;
+if [[ runprogs -ne 0 ]]; then exit 1; fi;
 make clean;
 
 exit 0;
